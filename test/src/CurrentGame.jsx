7efd16b6aec94f3e11b5e39ee74012860a8ceb90
Aaron Wong <aaronmwong0820@gmail.com>
2024-06-10 18:21:32 -0700

    Conditional rendering of score based on if game started.

--- test/src/CurrentGame.jsx
+++ test/src/CurrentGame.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './CurrentGame.module.css'
 import teamLogos from './teamLogos'
+import { useState, useEffect } from 'react';
 
 function CurrentGame({data}) {
+    const [gameStart, setGameStart] = useState(true);
+
+    useEffect(() => {
+        const gameStatus = data.summary.split(" ").pop();
+        setGameStart(gameStatus === "(Pre-Game)" || gameStatus === "(Warmup)");
+    }, [data]); // Dependency array ensures this effect runs only when `data` changes
+
     return (
         <div className={styles.container}>
             <div className={styles.score}>
@@ -10,14 +18,12 @@ function CurrentGame({data}) {
                     <img src={teamLogos[data.away_name]}/>
                     <p>{data.away_name.split(" ").pop()}</p>
                 </div>
-                <p>{`${data.away_score} - ${data.home_score}`}</p>
+                <p>{gameStart ? `${data.game_datetime.substring(11)}` : `${data.away_score} - ${data.home_score}`}</p>
                 <div className={styles.teamName}>
                     <img src={teamLogos[data.home_name]}/>
                     <p>{data.home_name.split(" ").pop()}</p>
                 </div>
-                    
             </div>
-            
         </div>
     )
 }
