beb3a6f4c5be971ec7845999a103d00075532548
Aaron Wong <aaronmwong0820@gmail.com>
2024-06-12 16:41:54 -0700

    Added header elements.

--- test/src/Header.jsx
+++ test/src/Header.jsx
@@ -3,7 +3,12 @@ import styles from './Header.module.css'
 
 function Header() {
     return (
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+            <nav>HOME</nav>
+            <nav>STANDINGS</nav>
+            <nav>SCHEDULE</nav>
+            <nav className={styles.right}>LOGIN</nav>
+        </div>
     )
 }
 
--- test/src/LeagueStanding.jsx
+++ test/src/LeagueStanding.jsx
@@ -5,9 +5,9 @@ function LeagueStanding({east, central, west}) {
     return (
         <div style={{display: `flex`}}>
             <DivisionStanding data={east}/>
-            <div style={{margin: `10px`}}></div>
+            <div style={{margin: `5px`}}></div>
             <DivisionStanding data={central}/>
-            <div style={{margin: `10px`}}></div>
+            <div style={{margin: `5px`}}></div>
             <DivisionStanding data={west}/>
         </div>
     )
