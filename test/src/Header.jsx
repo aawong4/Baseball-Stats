06446dae050eef49127a3b05aa85a7dbc6f1b8d1
Aaron Wong <aaronmwong0820@gmail.com>
2024-06-12 17:02:19 -0700

    Added baseball icon and changed title.

--- test/src/Header.jsx
+++ test/src/Header.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import styles from './Header.module.css'
 
 function Header() {
+    const redirectToMLB = () => {
+        window.location.href = "https://www.mlb.com";
+    }
+
     return (
         <div className={styles.container}>
-            <nav>HOME</nav>
+            <nav onClick={redirectToMLB}>HOME</nav>
             <nav>STANDINGS</nav>
             <nav>SCHEDULE</nav>
             <nav className={styles.right}>LOGIN</nav>
